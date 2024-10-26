27111c9576b2901008e05f9b629695a50b5c6d3d
seokmin <msm0748@gmail.com>
2024-10-26 23:17:36 +0900

    feat: selectedTool active 색상 추가

--- src/lib/components/working/header/Tool.svelte
+++ src/lib/components/working/header/Tool.svelte
@@ -7,12 +7,25 @@
 </script>
 
 <ul>
-	<li><button on:click={() => selectedTool.select('rectangle')}>그리기</button></li>
-	<li><button on:click={() => selectedTool.select('move')}>이동</button></li>
-	<li><button on:click={() => selectedTool.select('select')}>선택</button></li>
+	<li>
+		<button
+			on:click={() => selectedTool.select('rectangle')}
+			class:active={$selectedTool === 'rectangle'}>그리기</button
+		>
+	</li>
+	<li>
+		<button on:click={() => selectedTool.select('move')} class:active={$selectedTool === 'move'}
+			>이동</button
+		>
+	</li>
+	<li>
+		<button on:click={() => selectedTool.select('select')} class:active={$selectedTool === 'select'}
+			>선택</button
+		>
+	</li>
 	<li><button>뒤로 가기</button></li>
 	<li><button>앞으로 가기</button></li>
-	<li><button>코멘트</button></li>
+	<li><button class:active={$selectedTool === 'comment'}>코멘트</button></li>
 </ul>
 
 <style lang="scss">
@@ -24,6 +37,13 @@
 
 		button {
 			color: #fff;
+			padding: 5px;
+			border-radius: 3px;
+
+			&.active {
+				background-color: #ddd;
+				color: #000;
+			}
 		}
 	}
 </style>
